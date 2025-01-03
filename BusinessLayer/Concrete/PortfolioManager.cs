a58b0f04491eb7c3dfcc0c1022bc887739dabfb8
Kaan Kaya <kaankaya.0707@gmail.com>
2025-01-03 03:17:03 +0300

    Portfolio Service,Manager

---
+++ BusinessLayer/Concrete/PortfolioManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class PortfolioManager : IPortfolioService
+    {
+        IPortfolio _portfolio;
+
+        public PortfolioManager(IPortfolio portfolio)
+        {
+            _portfolio = portfolio;
+        }
+
+        public void TAdd(Portfolio t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(Portfolio t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Portfolio TGetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Portfolio> TGetList()
+        {
+            return _portfolio.GetList();
+        }
+
+        public void TUpdate(Portfolio t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MvcOnline/ViewComponents/Portfolio/PortfolioList.cs
@@ -0,0 +1,18 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcOnline.ViewComponents.Portfolio
+{
+    public class PortfolioList : ViewComponent
+    {
+        PortfolioManager manage = new PortfolioManager(new EfPortfolioDal());
+
+     
+        public IViewComponentResult Invoke()
+        {
+            var values = manage.TGetList();
+            return View(values);
+        }
+    }
+}
